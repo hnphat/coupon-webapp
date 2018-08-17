0d3b1434368b3ff13ac43b27d27d51b102a0ef20
hnphat <hnphat.ag@gmail.com>
2018-08-17 16:42:03 +0700

    The first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+	include "top.php";
+?>
+<title>Sale 4 mùa giá giảm mỗi ngày</title>
+<meta name="googlebot" content="code" />
+<meta name="robots" content="INDEX,FOLLOW" />
+<meta name="keywords" content="mã giảm giá, big sale, mua hàng giá rẻ, counpon, ma giam gia" />
+<meta name="description" content="Sale 4 mùa cung cấp mã giảm giá, các chương trình khuyến mãi, tổng hợp nhanh và chính xác." />
+<?php
+	include "top2.php";
+?>
+<!-- Body -->
+<h1>Đập xây lại</h1>
+<!-- Body -->
+<?php
+	include "bot.php"
+?>
---
+++ top2.php
@@ -0,0 +1,14 @@
+<!--<meta http-equiv="refresh" content="10" />-->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+<style>    
+    body{
+        font-family: verdana, sans-serif;
+    }  
+    /* Custom w3.css */
+    
+    /*---------------*/
+</style>
+</head>
+<body>
