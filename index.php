a8a6bcb0f68173323c1833c0b196ffa30f746d96
hnphat.ag <hnphat.ag@gmail.com>
2018-08-27 20:35:41 +0700

    update code

--- index.php
+++ index.php
@@ -10,9 +10,71 @@
 	include "top2.php";
 ?>
 <!-- Body -->
-<div id="main">
-    <h1>Hello</h1>
-    
+<div class="w3-container w3-black">	
+	<nav class="w3-bar w3-black main">
+    	<a href="#" class="w3-bar-item w3-mobile"><img class="sale-logo" src="sale4mua.png" alt="logo sale 4 mua"></a>
+    	<a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-black w3-hover-text-red"><span class="fa fa-home sale-bt-size"></span> HOME</a>
+    	<a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-black w3-hover-text-red"><span class="fa fa-certificate sale-bt-size"></span> COUNPON</a>
+    	<a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-black w3-hover-text-red"><span class="fa fa-codiepie sale-bt-size"></span> HOT DEAL</a>
+    	<a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-black w3-hover-text-red"><span class="fa fa-compass sale-bt-size"></span> SO SANH</a>
+    	<a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-black w3-hover-text-red"><span class="fa fa-cube sale-bt-size"></span> INTERVIEW</a>
+    	<a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-black w3-hover-text-red"><span class="fa fa-database sale-bt-size"></span> INFORMATION</a>
+	</nav>
+</div>
+<div class="main">	
+	<!-- 
+    <header>
+    	<h1 class="w3-center">This is a title for saling</h1>
+    	<p class="w3-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </header>
+    -->
+    <section class="w3-row">
+    	<div class="w3-third sale-center w3-padding">
+    		<a href="#"><img class="w3-image" src="image/topsale/test.jpg" alt="demo test1"></a>
+    	</div>
+    	<div class="w3-third sale-center w3-padding">
+    		<a href="#"><img class="w3-image" src="image/topsale/test5.jpg" alt="demo test1"></a>
+    	</div>
+    	<div class="w3-third sale-center w3-padding">
+    		<div class="sale-center">
+    			<img class="w3-image" src="image/topsale/test3.jpg" alt="demo test">
+    		</div><br/>
+    		<div class="sale-center">
+    			<img class="w3-image" src="image/topsale/test3.png" alt="demo test">
+    		</div>
+    	</div>
+    </section>
+    <section class="w3-row">
+    	<div class="w3-twothird w3-padding">
+    		<img class="w3-image" src="image/topsale/test4.jpg" alt="demo test">
+    	</div>
+    	<div class="w3-third w3-padding">
+    		<img class="w3-image" src="image/topsale/test2.jpg" alt="demo test">
+    	</div>
+    </section>
+    <main class="w3-row">
+    	<section class="w3-twothird w3-padding">
+    		<div class="w3-row">
+    			<article class="w3-twothird sale-padding-min">
+    				<img class="w3-image" src="image/topcontent/test1.jpg" alt="demotest" />
+    				<a class="sale-link" href="#"><h1 class="sale-h1-size"><strong>Trên tay Pocophone F1 chính hãng: 7.99 triệu có Snapdragon 845, pin 4000mAh, tai thỏ, camera kép</strong></h1></a>
+    				<p>Là sản phẩm đầu tiên của thương hiệu con của Xiaomi, Pocophone F1 là chiếc máy tầm trung nhưng với hiệu năng ngang ngửa các sản phẩm cao cấp.</p>
+    			</article>
+    			<article class="w3-third sale-padding-min">
+    				<img class="w3-image" src="image/topcontent/test2.jpg" alt="demotest" />
+    				<a class="sale-link" href="#"><h1 class="sale-h1-size-low"><strong>Dùng thử Bluetooth Receiver vô danh giá 40.000 đồng: Của rẻ mà không hề ôi!</strong></h1></a>
+    			</article>
+    		</div>    		
+    	</section>
+    	<aside class="w3-third w3-padding">
+    		
+    	</aside>
+    </main>
 </div>
 
 <!-- Body -->
--- top2.php
+++ top2.php
@@ -2,15 +2,76 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
-<style>    
+<style>   
+	* {
+		box-sizing: border-box;
+	} 
     body{
         font-family: verdana, sans-serif;
     }  
     /* Custom w3.css */
-    #main {
-        max-width: 1131px;
+    .main {
+        max-width: 1080px;
         margin: auto;
     }
+
+    .sale-reset {
+    	margin: 0;
+    	padding: 0;
+    }
+    .sale-center {
+    	display: block;
+    	margin-left: auto;
+    	margin-right: auto;
+    }
+
+    .sale-bold {
+    	font-weight: bold;
+    }
+
+    .sale-bt-size {
+    	font-size: 14pt;
+    }
+
+    .sale-logo {
+    	width: 70px;
+    	height: 30px;
+    }
+
+    .sale-image-100 {
+    	width: 100%;
+    }
+
+    .sale-image-small {
+    	width: 380px;
+    	height: 150px;    	
+    }
+
+    .sale-image-small img {
+
+    }
+    .clearfix::after {
+    	content: "";
+    	clear: both;
+    	display: table;
+
+    }
+    .sale-link {
+    	text-decoration: none;
+    	line-height: 22pt;
+    }
+    .sale-link:hover {
+    	color: DarkSlateGray;
+    }
+    .sale-h1-size {
+    	font-size: 18pt;
+    }
+    .sale-h1-size-low {
+    	font-size: 16pt;
+    }
+   	.sale-padding-min {
+   		padding: 5px;
+   	}
     /*---------------*/
 </style>
 </head>
